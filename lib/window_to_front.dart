af4ccdbf09d8ab9ddc35494a36aba9ed1b3e4fc8
Sumit Vekariya <sumitvekariya7@gmail.com>
2021-05-07 09:18:29 +0530

    File Formatted according to dart format

--- lib/window_to_front.dart
+++ lib/window_to_front.dart
@@ -8,11 +8,9 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class WindowToFront {
-  static const MethodChannel _channel =
-      const MethodChannel('window_to_front');
+  static const MethodChannel _channel = const MethodChannel('window_to_front');
 
   static Future<void> activate() async {
     await _channel.invokeMethod('activate');
   }
-
 }
